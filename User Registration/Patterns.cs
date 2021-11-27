69bc83177d019abf21f84e883599abf116fb46b5
“Varun13016” <dmevarun@gmail.com>
2021-11-27 15:46:23 +0530

    Add project files.

---
+++ User Registration/Patterns.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace User_Registration
+{
+    public class Patterns
+    {
+        public static void FirstName(string name)
+        {
+            string FirstName = "^[A-Z]\\w{1,10}$$";
+            Regex regex = new Regex(FirstName);
+            if (regex.IsMatch(name))
+            {
+                Console.WriteLine(name + " is valid name");
+            }
+            else
+            {
+                Console.WriteLine(name + " is invalid. Please Enter First name start with capital letter and maximum 10 charactors");
+            }
+        }
+    }
+}
---
+++ User Registration/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace User_Registration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Regular Expression Program \n");
+            Console.WriteLine("Write your name using 1st character should be Capital ");
+            Console.Write("\n Enter First Name : ");
+            string name = Console.ReadLine();
+            Patterns.FirstName(name);
+        }
+    }
+}
