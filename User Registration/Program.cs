b196ecdc8f50b80158e1315f7eb18169ab1565a1
“Varun13016” <dmevarun@gmail.com>
2021-11-27 16:30:53 +0530

    UC-8

--- User Registration/Program.cs
+++ User Registration/Program.cs
@@ -7,7 +7,7 @@ namespace User_Registration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Regular Expression Program \n");
-            Console.WriteLine("Write your Password ex : Aa9 length atleast 8");
+            Console.WriteLine("Write your Password ex : X@xxxxx123");
             Console.Write("\n Enter Password : ");
             string name = Console.ReadLine();
             Patterns.Password(name);
